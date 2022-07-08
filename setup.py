870d31d7d33b4eff58e41a7651ec24eed2a73927
blindedjoy <hnjoy@mac.com>
2022-07-08 09:20:53 -0600

    notebooks updated

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'rctorch',         # How you named your package folder (MyLib)
   packages = ['rctorch'],   # Chose the same as "name"
-  version = '0.7162',      # Start with a small number and increase it with every change you make
+  version = '0.8',      # Start with a small number and increase it with every change you make
   license='Harvard',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description='A Python 3 toolset for creating and optimizing Echo State Networks. This library is an extension and expansion of the previous library written by Reinier Maat: https://github.com/1Reinier/Reservoir',# Give a short description about your library
   author = 'Hayden Joy',                   # Type in your name
   author_email = 'hnjoy@mac.com',      # Type in your E-Mail
-  url = 'https://github.com/blindedjoy/RcTorch-private',   # Provide either the link to your github or to your website
+  url = 'https://github.com/blindedjoy/RcTorch',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/blindedjoy/RcTorch/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['Echo State Network', 'ESN', 'Reservoir Computing', 'Echo State Networks', 'Optimization', 'BoTorch', 'PyTorch', 'Bayesian'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
